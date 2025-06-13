83cd235b009fcc8d4a37483d8941b02621cd6368
Flavio <flaviocba@gmail.com>
2025-06-12 23:11:26 -0300

    Create temperatura.py

---
+++ day1/temperatura.py
@@ -0,0 +1,15 @@
+#Esse é o terceiro arquivo em python, seguindo o desafio numero 2 (dia 1 parte 3)
+#
+# Crie um conversor de Celsius para Fahrenheit
+# Fórmula: F = (C × 9/5) + 32
+# O programa deve:
+# 1. Pedir a temperatura em Celsius
+# 2. Converter para Fahrenheit
+# 3. Mostrar o resultado com 1 casa decimal
+
+# Exemplo:
+# "25°C = 77.0°F"
+
+temperatura = int(input("Qual a temperatura em Celsius? "))
+convertido = (temperatura*9/5)+32
+print(f"O valor de {temperatura} em Celsius convertido para Fahrenheit fica igual a {convertido}")
