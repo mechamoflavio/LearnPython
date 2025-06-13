76a880cd7f81cb8a5311c7382f4f611cc0f65c37
Flavio <flaviocba@gmail.com>
2025-06-12 22:56:30 -0300

    Initial commit

---
+++ day1/numeros.py
@@ -0,0 +1,29 @@
+# Meu segundo programa baseado no desafio 1 (dia 1 parte 3)
+#
+# Crie um programa que:
+# 1. Pede dois números ao usuário
+# 2. Calcula: soma, subtração, multiplicação e divisão
+# 3. Mostra os resultados formatados
+
+# Exemplo de saída:
+# "Soma: 15, Subtração: 5, Multiplicação: 50, Divisão: 2.0"
+
+#1
+
+primeiro_numero = int(input("Olá, diga 1 numero inteiro: "))
+segundo_numero = int(input("agora diga um 2 numero inteiro: "))
+
+#2
+
+soma = primeiro_numero + segundo_numero
+subtração = primeiro_numero - segundo_numero
+multiplicação = primeiro_numero * segundo_numero
+divisão = primeiro_numero / segundo_numero
+
+#3
+
+print(f"Aqui estão os resultados para os numeros {primeiro_numero} e {segundo_numero}")
+print(f"{primeiro_numero}+{segundo_numero}={soma}")
+print(f"{primeiro_numero}-{segundo_numero}={subtração}")
+print(f"{primeiro_numero}*{segundo_numero}={multiplicação}")
+print(f"{primeiro_numero}/{segundo_numero}={divisão}")
---
+++ day1/variaveis.py
@@ -0,0 +1,6 @@
+# Meu primeiro programa em Python
+nome = input("Qual seu nome? ")
+idade = int(input("Quantos anos você tem? "))
+ano_nascimento = 2025 - idade
+
+print(f"Olá {nome}! Você nasceu em {ano_nascimento}.")
