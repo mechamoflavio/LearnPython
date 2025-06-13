35e468e37dd66a96ede9f272f3e95e1af0f8f632
Flavio <flaviocba@gmail.com>
2025-06-13 10:16:53 -0300

    Create 07_desafio2.py
    Desafio2

---
+++ day2/07_desafio2.py
@@ -0,0 +1,8 @@
+###### Crie um jogo onde:
+###### 1. O computador sorteia um número entre 1-100
+###### 2. O usuário tem 7 tentativas para adivinhar
+###### 3. A cada tentativa, o programa diz se o número é maior ou menor
+###### 4. Ao acertar ou esgotar tentativas, mostra mensagem apropriada
+###### # Dica:
+###### #   import random
+###### #   numero_secreto = random.randint(1, 100)
