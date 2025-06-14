4f46334f6902069cbc3ea26d5112adf30da21fe8
Flavio <flaviocba@gmail.com>
2025-06-13 23:01:28 -0300

    Update 13_desafio1.py

--- aula 003/13_desafio1.py
+++ aula 003/13_desafio1.py
@@ -1,9 +1,9 @@
 ###### Desafio1.py
 ######
 ###### Crie um sistema para gerenciar sua coleção de jogos:
-###### 1. Use dicionário com gêneros como chaves
-###### 2. Cada gênero contém uma lista de jogos
-###### 3. Cada jogo é um dicionário com: título, ano, plataforma
+###### 1. Use dicionário com gêneros como chaves                        ok
+###### 2. Cada gênero contém uma lista de jogos                         ok
+###### 3. Cada jogo é um dicionário com: título, ano, plataforma        ok
 ###### 4. Implemente funções para:
 ######    - Adicionar jogo
 ######    - Listar jogos por plataforma
@@ -11,8 +11,8 @@
 ######    - Mostrar estatísticas (total de jogos, jogos por gênero)
 ######
 
-# database inicial
-biblioteca = { # condição 1: dicionario
+# database inicial de jogos
+db_jogos = { # condição 1: dicionario
     "Tiro/FPS": [ # condição 2: genero e lista
         {"titulo": "Counter-Strike", "ano": 1999, "plataforma": "PC"}, # condição 3: titulo, ano, plataforma
         {"titulo": "Call of Duty: Mobile", "ano": 2019, "plataforma": "Mobile"}
@@ -27,3 +27,22 @@ biblioteca = { # condição 1: dicionario
         {"titulo": "Real Racing 3", "ano": 2013, "plataforma": "Mobile"}
         ]
     }
+
+# função adicionar jogo
+def add_jogo():
+    genero = input("Gênero do jogo: ").lower()
+
+    titulo = input("Titulo: ")
+    plataforma = input("Plataforma: ")
+
+    try:
+        ano = int(input("Ano de lançamento: "))
+    except ValueError:
+        print("Ano invalido! Use apenas numeros.")
+        return
+
+    novo_jogo = (titulo, plataforma, ano) # tupla de dados
+
+    if genero not in db_jogos:
+        db_jogos[genero] = []
+
